746158713c7918db04a54512524d054e3c9da4be
Ko_Hein <heinhtet247232@gmail.com>
2019-06-11 11:27:08 +0630

    first commit

---
+++ src/main/java/tacos/controller/DesignTacoController.java
@@ -0,0 +1,12 @@
+package tacos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+	
+	
+
+}
